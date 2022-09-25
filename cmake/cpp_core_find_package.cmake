f2f776e35456f5b8fb7da5635b5585c8bfc37734
Mark Melton <mark.melton@notlem.com>
2022-09-25 17:17:14 -0400

    Updated Github builds to trigger on relevant changes.

--- cmake/cpp_core_find_package.cmake
+++ cmake/cpp_core_find_package.cmake
@@ -2,35 +2,56 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 
 function(cpp_core_find_package NAME)
   cmake_parse_arguments(CCFP
-    "" # options
-    "VERSION" # single-value arguments
+    "EXCLUDE_FROM_ALL" # options
+    "URL;VERSION" # single-value arguments
     "" # multi-value arguments
     ${ARGN}
     )
-  message("-- cpp_core_find_package NAME: ${NAME}")
-  message("-- cpp_core_find_package VERSION: ${CCFP_VERSION}")
-  
-  include(FetchContent)
+
+  if (DEFINED CCFP_URL)
+    set(URL ${CCFP_URL})
+  else()
+    set(URL https://github.com/cpp-core/${NAME})
+  endif()
 
   if (DEFINED CCFP_VERSION)
-    FetchContent_Declare(
-      ${NAME}
-      GIT_REPOSITORY https://github.com/cpp-core/${NAME}
-      GIT_TAG ${CCFP_VERSION}
-      GIT_SHALLOW TRUE
-      FIND_PACKAGE_ARGS
-      )
+    set(VERSION ${CCFP_VERSION})
   else()
-    FetchContent_Declare(
-      ${NAME}
-      GIT_REPOSITORY https://github.com/cpp-core/${NAME}
-      GIT_TAG main
-      GIT_SHALLOW TRUE
-      FIND_PACKAGE_ARGS
-      )
+    set(VERSION main)
+  endif()
+
+  if (${CCFP_EXCLUDE_FROM_ALL})
+    set(EXCLUDE_FROM_ALL EXCLUDE_FROM_ALL)
   endif()
   
-  FetchContent_MakeAvailable(${NAME})
+  message("-- cpp_core_find_package NAME: ${NAME}")
+  message("-- cpp_core_find_package URL: ${URL}")
+  message("-- cpp_core_find_package VERSION: ${VERSION}")
+  message("-- cpp_core_find_package EXCLUDE_FROM_ALL: ${EXCLUDE_FROM_ALL}")
+  
+  include(FetchContent)
+  
+  FetchContent_Declare(
+    ${NAME}
+    GIT_REPOSITORY ${URL}
+    GIT_TAG ${VERSION}
+    GIT_SHALLOW TRUE
+    FIND_PACKAGE_ARGS
+    )
+
+  find_package(${NAME} QUIET)
+  
+  if (TARGET ${NAME} OR TARGET ${NAME}::${NAME})
+    return()
+  endif()
+
+  FetchContent_GetProperties(${NAME})
+  string(TOLOWER "${NAME}" lc_name)
+  
+  if (NOT ${lc_name}_POPULATED)
+    FetchContent_Populate(${NAME})
+    add_subdirectory(${${lc_name}_SOURCE_DIR} ${${lc_name}_BINARY_DIR} ${EXCLUDE_FROM_ALL})
+  endif()
   
 endfunction()
 
